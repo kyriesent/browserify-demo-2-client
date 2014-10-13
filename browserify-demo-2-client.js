aa269974316ed6d2cfad49b6c34d8b2c3ddcc980
David Jsa <kiesent@gmail.com>
2014-10-13 13:18:05 -0400

    add a super secret hashgit add .

--- browserify-demo-2-client.js
+++ browserify-demo-2-client.js
@@ -3,3 +3,15 @@ socket.on('connect', function(){
   socket.on('event', function(data){});
   socket.on('disconnect', function(){});
 });
+
+var supersecrethash = require('./supersecrethash')
+
+var Client = function () {}
+
+Client.prototype.send = function(text) {
+  socket.emit('client text', supersecrethash, text);
+};
+
+var client = new Client
+
+module.exports = client
